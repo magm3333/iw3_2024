fae8fdbad29eef5fb83dc7431e328373e907f93e
magm3333 <magm3333@gmail.com>
2024-09-04 08:49:55 -0300

    3-9-2024

--- src/main/java/ar/edu/iw3/controllers/ProductRestController.java
+++ src/main/java/ar/edu/iw3/controllers/ProductRestController.java
@@ -1,27 +1,38 @@
 package ar.edu.iw3.controllers;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ar.edu.iw3.model.Product;
 import ar.edu.iw3.model.business.BusinessException;
+import ar.edu.iw3.model.business.FoundException;
 import ar.edu.iw3.model.business.IProductBusiness;
+import ar.edu.iw3.model.business.NotFoundException;
 import ar.edu.iw3.util.IStandartResponseBusiness;
 
 @RestController
 @RequestMapping(Constants.URL_PRODUCTS)
 public class ProductRestController extends BaseRestController {
-
+	// /produtos?precio=100# /a/?/c/d//////?param=valor&param1=valor&param=valor1
 	@Autowired
 	private IProductBusiness productBusiness;
-	
+
 	@Autowired
 	private IStandartResponseBusiness response;
-	
+
 	@GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> list() {
 		try {
@@ -31,7 +42,66 @@ public class ProductRestController extends BaseRestController {
 					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
-	
-	
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> load(@PathVariable long id) {
+		try {
+			return new ResponseEntity<>(productBusiness.load(id), HttpStatus.OK);
+		} catch (BusinessException e) {
+			return new ResponseEntity<>(response.build(HttpStatus.INTERNAL_SERVER_ERROR, e, e.getMessage()),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (NotFoundException e) {
+			return new ResponseEntity<>(response.build(HttpStatus.NOT_FOUND, e, e.getMessage()), HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@GetMapping(value = "/by_name/{product}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> load(@PathVariable String product) {
+		try {
+			return new ResponseEntity<>(productBusiness.load(product), HttpStatus.OK);
+		} catch (BusinessException e) {
+			return new ResponseEntity<>(response.build(HttpStatus.INTERNAL_SERVER_ERROR, e, e.getMessage()),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (NotFoundException e) {
+			return new ResponseEntity<>(response.build(HttpStatus.NOT_FOUND, e, e.getMessage()), HttpStatus.NOT_FOUND);
+		}
+	}
+	@PostMapping(value = "")
+	public ResponseEntity<?> add(@RequestBody Product product) {
+		try {
+			Product response = productBusiness.add(product);
+			HttpHeaders responseHeaders = new HttpHeaders();
+			responseHeaders.set("location", Constants.URL_PRODUCTS + "/" + response.getId());
+			return new ResponseEntity<>(responseHeaders, HttpStatus.CREATED);
+		} catch (BusinessException e) {
+			return new ResponseEntity<>(response.build(HttpStatus.INTERNAL_SERVER_ERROR, e, e.getMessage()),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (FoundException e) {
+			return new ResponseEntity<>(response.build(HttpStatus.FOUND, e, e.getMessage()), HttpStatus.FOUND);
+		}
+	}
+	@PutMapping(value = "")
+	public ResponseEntity<?> update(@RequestBody Product product) {
+		try {
+			productBusiness.update(product);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (BusinessException e) {
+			return new ResponseEntity<>(response.build(HttpStatus.INTERNAL_SERVER_ERROR, e, e.getMessage()),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (NotFoundException e) {
+			return new ResponseEntity<>(response.build(HttpStatus.NOT_FOUND, e, e.getMessage()), HttpStatus.NOT_FOUND);
+		}
+	}
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable long id) {
+		try {
+			productBusiness.delete(id);
+			return new ResponseEntity<String>(HttpStatus.OK);
+		} catch (BusinessException e) {
+			return new ResponseEntity<>(response.build(HttpStatus.INTERNAL_SERVER_ERROR, e, e.getMessage()),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (NotFoundException e) {
+			return new ResponseEntity<>(response.build(HttpStatus.NOT_FOUND, e, e.getMessage()), HttpStatus.NOT_FOUND);
+		}
+	}
 }
